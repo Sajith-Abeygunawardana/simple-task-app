80e6f6dc3eb7bef40ecd88a099777e6d2e5aba66
Sajith Abeygunawardana <abeygunawardana1995@gmail.com>
2023-08-04 16:56:45 +0530

    implement the add task

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,21 @@ import './App.css';
 
 function App() {
     const [taskList,setTaskList] = useState(Array<string>);
-    const [value,setValue]=useState();
+    const [value,setValue]=useState('');
 
   return (
       <>
         <h1 className={"text-center p-2"}>Simple Task App</h1>
           <div className={"d-flex p-2 gap-2"}>
-              <input className={"form-control"} type="text" placeholder={"Enter Task"}/>
+              <input value={value} onInput={(e)=>{
+                  setValue((e.target as HTMLInputElement).value)
+              }} className={"form-control"} type="text" placeholder={"Enter Task"}/>
               <button className={"btn btn-primary"} onClick={()=>{
-                setTaskList([...taskList,"task1"]);
+                setTaskList([...taskList,value]);
               }}>ADD</button>
           </div>
           <div>
-              {taskList.map(()=><div>task1</div>)}
+              {taskList.map((task)=><div className={'p-2 m-2'}>{task}</div>)}
           </div>
       </>
   );
