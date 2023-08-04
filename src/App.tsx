ace194fa5d68286343009e87358689f71e9fdd54
Sajith Abeygunawardana <abeygunawardana1995@gmail.com>
2023-08-04 16:31:03 +0530

    created the adding task feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+    const [taskList,setTaskList] = useState(Array<string>);
+    const [value,setValue]=useState();
+
   return (
       <>
-        <h1>Simple Task App</h1>
+        <h1 className={"text-center p-2"}>Simple Task App</h1>
+          <div className={"d-flex p-2 gap-2"}>
+              <input className={"form-control"} type="text" placeholder={"Enter Task"}/>
+              <button className={"btn btn-primary"} onClick={()=>{
+                setTaskList([...taskList,"task1"]);
+              }}>ADD</button>
+          </div>
+          <div>
+              {taskList.map(()=><div>task1</div>)}
+          </div>
       </>
   );
 }
